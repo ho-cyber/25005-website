57649ad515ca4d7fad13e224c0fda541e6b296a6
Dhruv Pai Dukle <dhruvpaidukle@Dhruvs-MacBook-Pro.local>
2025-01-11 22:54:06 +0530

    1

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -100,7 +100,7 @@ const Experience = () => {
   const partners = [
     { src: "/assets/LegoGoa.svg", alt: "LegoGoa" },
     { src: "/assets/PRATHAM.svg", alt: "Partner 2" },
-    { src: "/assets/logo-color.png", alt: "Partner 3" },
+    { src: "/assets/logo-color.svg", alt: "Partner 3" },
   ];
 
   return (
